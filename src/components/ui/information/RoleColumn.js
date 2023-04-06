22cc252369bc42107a67aa30e614c3efa8f991b0
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2023-04-06 17:18:53 +0200

    #48, #132 view of all role stand's almost

--- src/components/ui/information/RoleColumn.js
+++ src/components/ui/information/RoleColumn.js
@@ -2,10 +2,19 @@ import PropTypes from 'prop-types';
 
 const RoleColumn = ({roles}) => {
 
-  return (
-    <div className= {`role-information`}>
-      <h5>Roles this game</h5>
+  const createIndividualRole = (role) => {
+    return (
+      <div className="individual-role">
+        <h2>{role.rolename}</h2>
+        <img src={`/public/roles/${role.rolename}.png`} alt={"Picture of a " + role.rolename}/>
+        <h5>{role.uid.length + "x"}</h5>
+      </div>
+      );
 
+  }
+  return (
+    <div className= {`roles-column`}>
+      {roles.map(role => (createIndividualRole(role)))}
     </div>
   );
 };
