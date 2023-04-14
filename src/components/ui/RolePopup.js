8775f3dbb1c57496fd59033f2b05aeaed1057bd7
Atomis14 <michelsabbatini@outlook.com>
2023-04-14 16:05:03 +0200

    #49 create popable div

---
+++ src/components/ui/RolePopup.js
@@ -0,0 +1,11 @@
+import 'styles/ui/PopupRole.scss';
+
+const RolePopup = () => {
+  return (
+    <div className='popup-role'>
+      POPUP ROLE
+    </div>
+  )
+}
+
+export default RolePopup;
--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -1,9 +1,22 @@
+import RolePopup from 'components/ui/RolePopup';
+import { useEffect, useState } from 'react';
 import 'styles/views/Game.scss';
 
 const Game = () => {
+  const [popupActive, setPopupActive] = useState(true);
+
+  const hidePopup = (e) => {
+    if(e.target.classList.contains('info-button')) return;
+    setPopupActive(false);
+  }
+  const togglePopup = () => {
+    setPopupActive(!popupActive);
+  }
+
   return (
-    <div className="background background-dark game">
-      <div className='info-button info-button-light'>i</div>
+    <div className="background background-dark game" onClick={hidePopup}>
+      <div className='info-button info-button-light' onClick={togglePopup}>i</div>
+      {popupActive && <RolePopup  />}
       <div className="container">
         <div>Game</div>
       </div>
