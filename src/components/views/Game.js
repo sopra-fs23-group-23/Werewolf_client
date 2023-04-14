938290b4d9bc6ce6e0e40b0cc897551fd0269f98
Atomis14 <michelsabbatini@outlook.com>
2023-04-14 15:36:04 +0200

    #49 create info button

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -3,6 +3,7 @@ import 'styles/views/Game.scss';
 const Game = () => {
   return (
     <div className="background background-dark game">
+      <div className='info-button info-button-light'>i</div>
       <div className="container">
         <div>Game</div>
       </div>
