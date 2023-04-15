6b1e1e6ddb201d03754193f49a7ab4067af0eede
Atomis14 <michelsabbatini@outlook.com>
2023-04-15 15:26:37 +0200

    #49 create HTML structure for role popup

--- src/components/ui/RolePopup.js
+++ src/components/ui/RolePopup.js
@@ -1,18 +1,42 @@
+import { useState, useEffect } from 'react';
 import { api } from 'helpers/api';
-import 'styles/ui/PopupRole.scss';
+import 'styles/ui/RolePopup.scss';
 import StorageManager from "../../helpers/StorageManager";
+import SingleRoleInformation from './SingleRoleInformation';
 
 const RolePopup = ({ show }) => {
   const id = StorageManager.getUserId();
   const lobbyId = StorageManager.getLobbyId();
+  const [allRoles, setAllRoles] = useState([]);
+  const [ownRoles, setOwnRoles] = useState([]);
 
-  const allRoles = [];
-  const ownRole = [];
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const responseAll = await api.get('/lobbies/' + lobbyId + "/roles");
+        setAllRoles(responseAll.data);
+        const responseOwn = await api.get('/lobbies/' + lobbyId + "/roles/" + id);
+        setOwnRoles(responseOwn.data);
+      } catch (error) {
+        alert('Could not fetch role information' + error.response.data?.message);
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [id, lobbyId]);
 
   if(show) {
     return (
-      <div className='popup-role background-dark'>
-        Your Role:
+      <div className='role-popup background-dark'>
+        {allRoles.map(role => 
+          <div className='role-popup-item' key={role.roleName}>
+            {(role.roleName === ownRoles[0].roleName) && <div>Your Role:</div>}
+            <h2 className='role-popup-item-title'>{ role.roleName }</h2>
+            <div className='role-popup-item-description'>{ role.description }</div>
+            <div className='role-popup-item-amount'>{ role.amount }</div>
+          </div>
+        )}
+        <div className='role-popup-nav'>Back | Forward</div>
       </div>
     );
   }
