e08f6d265854b104617aaacd6de372def6fdb285
djscherrer <davidjscherrer@outlook.com>
2023-04-22 12:23:53 +0200

    Implemented VoteMap for hitlist, Issues #26, #27, #81

--- src/hooks/Game.hooks.js
+++ src/hooks/Game.hooks.js
@@ -2,6 +2,7 @@ import StorageManager from "helpers/StorageManager";
 import { createEventSource } from "helpers/createEventSource";
 import { useCallback, useEffect, useState} from "react";
 import LobbyModel from "models/Lobby";
+import Player from "models/Player";
 
 export const useGame = () => {
     const lobbyId = StorageManager.getLobbyId();
@@ -13,78 +14,36 @@ export const useGame = () => {
     const [voteMap, setVoteMap] = useState(new Map());
     const [hitlist, setHitlist] = useState([]);
 
-    const MockPollOptions = [
-      {
-        "player": {
-          "id": 1,
-          "name": "David",
-          "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=1",
-          "alive": true
-        },
-        "supporters": [
-          {
-            "id": 2,
-            "name": "Miro",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=2",
-            "alive": true
-          },
-          {
-            "id": 3,
-            "name": "Jan",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=3",
-            "alive": true
-          },
-          {
-            "id": 4,
-            "name": "Marvin",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=4",
-            "alive": true
-          },
-          {
-            "id": 5,
-            "name": "Michel",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=5",
-            "alive": true
-          },
-          {
-            "id": 6,
-            "name": "Rudi",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=6",
-            "alive": true
-          },
-          {
-            "id": 7,
-            "name": "Balthasar",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=7",
-            "alive": true
-          },
-          {
-            "id": 8,
-            "name": "Salamander",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=8",
-            "alive": true
-          },
-          {
-            "id": 9,
-            "name": "Roland",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=9",
-            "alive": true
-          },
-          {
-            "id": 10,
-            "name": "Matthias",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=10",
-            "alive": true
-          },
-          {
-            "id": 11,
-            "name": "ChlineSaurus",
-            "avatarUrl": "https://api.dicebear.com/6.x/miniavs/svg?seed=11",
-            "alive": true
-          }
-        ]
-      }
-    ];
+    const MockPollOptions = {"question":"Who do you suspect to be a werewolf?","participants":[{"id":1,"name":"David","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=1","alive":true},{"id":2,"name":"Miro","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=2","alive":true},{"id":3,"name":"Jan","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=3","alive":true},{"id":4,"name":"Marvin","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=4","alive":true},{"id":5,"name":"Michel","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=5","alive":true},{"id":6,"name":"Rudi","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=6","alive":true},{"id":7,"name":"Balthasar","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=7","alive":true},{"id":8,"name":"Salamander","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=8","alive":true},{"id":9,"name":"Roland","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=9","alive":true},{"id":10,"name":"Matthias","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=10","alive":true},{"id":11,"name":"ChlineSaurus","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=11","alive":true}],
+    "pollOptions":[
+      {"player":{"id":1,"name":"David","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=1","alive":true},
+      "supporters":[
+        {"id":2,"name":"Mike","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=12","alive":true},
+        {"id":3,"name":"Emily","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=13","alive":true},
+        {"id":4,"name":"Sarah","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=14","alive":true}
+      ]},
+        {"player":{"id":2,"name":"Miro","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=2","alive":true},
+        "supporters":[
+          {"id":5,"name":"John","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=15","alive":true},
+          {"id":6,"name":"Linda","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=16","alive":true},
+          {"id":8,"name":"Yoda","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=16","alive":true},
+          {"id":7,"name":"Nina","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=17","alive":true}
+        ]},
+        {"player":{"id":5,"name":"John","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=2","alive":true},
+        "supporters":[
+          {"id":5,"name":"John","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=15","alive":true},
+          {"id":6,"name":"Linda","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=16","alive":true},
+        ]},
+        {"player":{"id":6,"name":"Linda","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=2","alive":true},
+        "supporters":[
+          {"id":5,"name":"John","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=15","alive":true},
+          {"id":6,"name":"Linda","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=16","alive":true},
+          {"id":1,"name":"David","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=15","alive":true},
+          {"id":2,"name":"Röbi","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=16","alive":true},
+          {"id":7,"name":"Nina","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=17","alive":true}
+        ]}
+        ]};
+
     const updateDataToLobby = useCallback((data) => {
       console.log("updateDataToGame", data.lobby)
       const lobby = new LobbyModel(data.lobby);
@@ -95,24 +54,20 @@ export const useGame = () => {
     const updateVoteMap = useCallback((data) => {
       data = MockPollOptions;
       const newVoteMap = new Map();
-      console.log("updateVoteMap in Game", data.pollOptions)
-      data.pollOptions.forEach(option => {
+      //console.log("updateVoteMap in Game XXX", data)
+      const sortedPollOptions = data.pollOptions.sort((a, b) => {
+        return b.supporters.length - a.supporters.length;
+      });
+      sortedPollOptions.forEach(option => {
+        let supporterArray = [];
         option.supporters.forEach(supporter => {
-          newVoteMap.set(option.player.id, supporter.id)
-          console.log(option.player.id, "votes for (Mock)", supporter.id)
+          supporterArray.push(supporter.id)
         })
-      });
-      
-      lobby.players.forEach(player => {
-        if (player.id < 4) {
-          newVoteMap.set(player.id, lobby.players[0].id)
-        } else if (player.id < 7) {
-          newVoteMap.set(player.id, lobby.players[1].id)
-        } else {
-          newVoteMap.set(player.id, lobby.players[2].id)
-        }
-      });
+        //console.log("VoteMap SET: ", new Player(option.player), supporterArray)
+        newVoteMap.set(new Player(option.player), supporterArray)
 
+      });
+      console.log("VoteMap final: ", newVoteMap)
       setVoteMap(newVoteMap);
     }, []);
 
@@ -150,20 +105,19 @@ export const useGame = () => {
         eventSource.addEventListener("start", (event) => {
             setStarted(true);
         });
+
         eventSource.addEventListener("stage", (event)=>{
           console.log("Stage started:", event.data);
           updateDataToLobby(JSON.parse(event.data));
-
-
         })
+
         eventSource.addEventListener("poll", (event)=>{
           console.log("Poll started:", event.data);
           updateVoteMap(JSON.parse(event.data));
-          updateHitlist(JSON.parse(event.data));
           //print key value pairs of votemap
-          for (const [key, value] of voteMap.entries()) {
-            console.log(`${key} votes for ${value}`);
-          }
+          // for (const [key, value] of voteMap.entries()) {
+          //   console.log(`${key} votes for ${value}`);
+          // }
         })
     }, [setStarted]);
 
