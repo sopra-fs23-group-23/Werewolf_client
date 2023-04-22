514c3543f6c64c0ed96c39fb6d007aaa1ef59af0
djscherrer <davidjscherrer@outlook.com>
2023-04-22 21:22:38 +0200

    Refactored Game into Stage

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -4,6 +4,7 @@ import { useGame } from 'hooks/Game.hooks';
 import 'styles/views/Game.scss';
 import { Information } from '../ui/game/Information';
 import Endscreen from '../ui/game/Endscreen';
+import Stage from '../ui/game/Stage';
 import Profile from '../ui/Profile';
 import Player from 'models/Player';
 import Countdown from '../ui/Countdown';
@@ -25,21 +26,7 @@ const Game = () => {
     ]
   };
 
-  console.log("EndData: ", MockEndData);
-
-
   const {started, stage, lobby, admin, voteMap, votingParty, question, voteParticipants, scheduledFinish, finished, endData} = useGame();
-
-  console.log("VotingParty: ", votingParty);
-  const voteArray = Array.from(voteMap);
-  console.log("VoteArray length: ", voteArray.length);
-  console.log("Finish: ", scheduledFinish);
-  // console.log("VALUE: ", voteArray[0][1].length);
-
-  //loop over voteMap and create a new array with the player and the amount of votes
-  //sort the array by the amount of votes
-  //take the first 5 players and put them in the hitlist
-  //take the first player and put him in the leader position
 
   let backgroundTheme = "dark";
   let textTheme = "light"
@@ -48,58 +35,19 @@ const Game = () => {
     textTheme = "dark"
   }
 
-  const castVote = async (optionId) => {
-    console.log("I clicked person: " + optionId);
-  };
-
   var content = Information();
+
   if (started) {
     content = (
-      <div className="container game">
-        <div className="game-stage-info">
-          <h1>{votingParty}</h1>
-          <p>{question}</p>
-        </div>
-        <div className="game-hitlist">
-          <Hitlist voteArray={voteArray} />
-        </div>
-        <div className="game-player-selection">
-          {lobby.players.map(player => (
-            <Profile user={new Player(player)} mode="selection" onClickEvent={castVote} />
-          ))}
-        </div>
-
-        <div className="game-stage-counter">
-          <Countdown finishTime={scheduledFinish} />
-        </div>
-
-        <div className={`game-dead-players game-dead-players-${backgroundTheme}`}>
-          {lobby.players.map(player => (
-            (!player.alive) && (
-              <Profile user={new Player(player)} mode="dead-player"/>
-            )
-          ))}
-        </div>
-
-      </div>
-    );
-    content = (
-      <Endscreen endData={MockEndData} lobby={lobby} />
+      <Stage votingParty={votingParty} question={question} voteMap={voteMap} voteParticipants={voteParticipants} lobby={lobby} scheduledFinish={scheduledFinish} admin={admin} stage={stage} />
     );
   }
-
-
-
-
-
 
   if (finished) {
     content = (
       <Endscreen endData={MockEndData} lobby={lobby} />
     );
   }
-
-  let playerMock = { id: "1234", name: "Willy", isAlive: true, avatarUrl: "https://api.dicebear.com/6.x/miniavs/svg?seed=2" };
 
   const [popupActive, setPopupActive] = useState(false);
 
