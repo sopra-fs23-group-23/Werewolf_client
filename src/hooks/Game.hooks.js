1b8c7e9fff80af7e2aa7c4bfc036023ff0eef7cc
djscherrer <davidjscherrer@outlook.com>
2023-04-22 13:55:37 +0200

    processed all Information from backend to use in frontend during game :)

--- src/hooks/Game.hooks.js
+++ src/hooks/Game.hooks.js
@@ -12,7 +12,8 @@ export const useGame = () => {
     const [lobby, setLobby] = useState(null);
     const [admin, setAdmin] = useState(false);
     const [voteMap, setVoteMap] = useState(new Map());
-    const [hitlist, setHitlist] = useState([]);
+    const [voteParticipants, setVoteParticipants] = useState([]);
+    const [scheduledFinish, setScheduledFinish] = useState(null);
 
     const MockPollOptions = {"question":"Who do you suspect to be a werewolf?","participants":[{"id":1,"name":"David","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=1","alive":true},{"id":2,"name":"Miro","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=2","alive":true},{"id":3,"name":"Jan","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=3","alive":true},{"id":4,"name":"Marvin","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=4","alive":true},{"id":5,"name":"Michel","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=5","alive":true},{"id":6,"name":"Rudi","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=6","alive":true},{"id":7,"name":"Balthasar","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=7","alive":true},{"id":8,"name":"Salamander","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=8","alive":true},{"id":9,"name":"Roland","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=9","alive":true},{"id":10,"name":"Matthias","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=10","alive":true},{"id":11,"name":"ChlineSaurus","avatarUrl":"https://api.dicebear.com/6.x/miniavs/svg?seed=11","alive":true}],
     "pollOptions":[
@@ -71,34 +72,15 @@ export const useGame = () => {
       setVoteMap(newVoteMap);
     }, []);
 
-    // const updateVoteMap = useCallback((data) => {
-    //   console.log("updateVoteMap in Game", data.pollOptions)
-    //   const lobby = new LobbyModel(data.lobby);
-    //   const newVoteMap = new Map();
-    //   lobby.players.forEach(player => {
-    //     if (player.id < 4) {
-    //       newVoteMap.set(player.id, lobby.players[0].id)
-    //     } else if (player.id < 7) {
-    //       newVoteMap.set(player.id, lobby.players[1].id)
-    //     } else {
-    //       newVoteMap.set(player.id, lobby.players[2].id)
-    //     }
-    //  });
-    //   setVoteMap(newVoteMap);
+    const updateVoteParticipants = useCallback((data) => {
+      let participantsArray = [];
+      data.participants.forEach(player => {
+        participantsArray.push(new Player(player));
+      });
+      setVoteParticipants(participantsArray);
+      console.log("Vote Participants: ", voteParticipants)
+    }, []);
 
-    // // }, []);
-
-    const updateHitlist = useCallback(() => {
-
-      const newHitlist = [];
-
-      for (const [key, value] of voteMap.entries()) {
-        if (key === value) {
-          newHitlist.push(key);
-        }
-      }
-      setHitlist(newHitlist);
-    }, [voteMap]);
 
     const subscribeToEmitter = useCallback((lobbyId, token) =>{
         const eventSource = createEventSource(`games/${lobbyId}/sse/${token}`);
@@ -108,12 +90,20 @@ export const useGame = () => {
 
         eventSource.addEventListener("stage", (event)=>{
           console.log("Stage started:", event.data);
-          updateDataToLobby(JSON.parse(event.data));
+          const dataJSON = JSON.parse(event.data);
+          updateDataToLobby(dataJSON);
+          setStage(dataJSON.stage.type);
+          setAdmin(new Player(dataJSON.admin));
         })
 
         eventSource.addEventListener("poll", (event)=>{
           console.log("Poll started:", event.data);
-          updateVoteMap(JSON.parse(event.data));
+          const dataJSON = JSON.parse(event.data);
+          updateVoteMap(dataJSON);
+          updateVoteParticipants(dataJSON);
+          setScheduledFinish(new Date(dataJSON.scheduledFinish));
+          console.log("scheduledFinish: ", new Date(scheduledFinish));
+
           //print key value pairs of votemap
           // for (const [key, value] of voteMap.entries()) {
           //   console.log(`${key} votes for ${value}`);
@@ -133,5 +123,5 @@ export const useGame = () => {
         subscribeToEmitter,
       ]);
 
-    return {started, stage, lobby, admin, voteMap, hitlist};
+    return {started, stage, lobby, admin, voteMap, voteParticipants, scheduledFinish};
 }
