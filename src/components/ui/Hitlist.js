0b1d5e52aa2ab9f98d2dc780dd91272935d6dcf3
djscherrer <davidjscherrer@outlook.com>
2023-04-25 09:46:22 +0200

    Closes #26, finished displaying who voted for whom (Should be tested with more supporters though)

--- src/components/ui/Hitlist.js
+++ src/components/ui/Hitlist.js
@@ -10,46 +10,29 @@ const Hitlist = ({voteMap}) => {
 
     const voteArray = Array.from(voteMap);
 
-    const [hoveredPlayer, setHoveredPlayer] = React.useState(null);
+    let simpleMap = new Map();
+    voteMap.forEach((value, key) => {
+        simpleMap.set(key.id, value);
+    });
 
-    useEffect(() => {
-        let simpleMap = new Map();
-        voteMap.forEach((value, key) => {
-            simpleMap.set(key.id, value);
-        });
-
-
+    const updateHoveredPlayer = (hoveredPlayer) => {
         let allPlayers = document.getElementsByClassName("profile-selection")
-        
-        for (let i = 0; i < allPlayers.length; i++) {
-            if (hoveredPlayer !== null){
-                console.log("Adding isNotVoter to: ", allPlayers[i])
-                allPlayers[i].classList.add("profile-selection-isNotVoter");
-            } else {
-                console.log("Removing isNotVoter from: ", allPlayers[i])
+        if (hoveredPlayer !== null){
+            let supporterArray = simpleMap.get(hoveredPlayer.id);
+            for (let i = 0; i < allPlayers.length; i++) {           
+                let playerId = parseInt(allPlayers[i].id.substring(18));
+                if (!supporterArray.includes(playerId)){
+                    allPlayers[i].classList.add("profile-selection-isNotVoter");
+                }
+            }
+        } else {
+            for (let i = 0; i < allPlayers.length; i++) {
                 allPlayers[i].classList.remove("profile-selection-isNotVoter");
             }
         }
-        
-        let supporterArray = [];
-        if (hoveredPlayer !== null){
-            supporterArray = simpleMap.get(hoveredPlayer.id);
-            supporterArray.forEach(supporter => {
-                let supporterProfile = document.getElementById(`profile-selection-${hoveredPlayer.id}`);
-                console.log("Adding isVoter to: ", supporterProfile)
-                supporterProfile.classList.add("profile-selection-isVoter");
-                
-            });
-        } else {
-            supporterArray = document.getElementsByClassName("profile-selection-isVoter");
-            for (let i = 0; i < supporterArray.length; i++) {
-                console.log("Removing isVoter from: ", supporterArray[i])
-                supporterArray[i].classList.remove("profile-selection-isVoter");
-            }
-        }
+    };
 
-        
-    }, [hoveredPlayer]);
+    
 
     const castVote = async (optionId) => {
         console.log("I clicked person: " + optionId);
@@ -59,23 +42,23 @@ const Hitlist = ({voteMap}) => {
         <div className="hitlist">
             <div className="hitlist-left">
                 { 4 <= voteArray.length ? (
-                <Profile user={new Player(voteArray[3][0])} mode="hitlist" votes={voteArray[3][1].length} onHoverEvent={setHoveredPlayer} onClickEvent={castVote}/>
+                <Profile user={new Player(voteArray[3][0])} mode="hitlist" votes={voteArray[3][1].length} onHoverEvent={updateHoveredPlayer} onClickEvent={castVote}/>
                 ) : null}
                 { 2 <= voteArray.length ? (
-                <Profile user={new Player(voteArray[1][0])} mode="hitlist" votes={voteArray[1][1].length} onHoverEvent={setHoveredPlayer} onClickEvent={castVote}/>
+                <Profile user={new Player(voteArray[1][0])} mode="hitlist" votes={voteArray[1][1].length} onHoverEvent={updateHoveredPlayer} onClickEvent={castVote}/>
                 ) : null}
             </div>
             <div className="hitlist-leader">
                 { 1 <= voteArray.length ? (
-                <Profile user={new Player(voteArray[0][0])} mode="hitlist-leader" votes={voteArray[0][1].length} onHoverEvent={setHoveredPlayer} onClickEvent={castVote}/>
+                <Profile user={new Player(voteArray[0][0])} mode="hitlist-leader" votes={voteArray[0][1].length} onHoverEvent={updateHoveredPlayer} onClickEvent={castVote}/>
                 ) : null}          
             </div>
             <div className="hitlist-right">
                 { 3 <= voteArray.length ? (
-                <Profile user={new Player(voteArray[2][0])} mode="hitlist" votes={voteArray[2][1].length} onHoverEvent={setHoveredPlayer} onClickEvent={castVote}/>
+                <Profile user={new Player(voteArray[2][0])} mode="hitlist" votes={voteArray[2][1].length} onHoverEvent={updateHoveredPlayer} onClickEvent={castVote}/>
                 ) : null}
                 { 5 <= voteArray.length ? (
-                <Profile user={new Player(voteArray[4][0])} mode="hitlist" votes={voteArray[4][1].length} onHoverEvent={setHoveredPlayer} onClickEvent={castVote}/>
+                <Profile user={new Player(voteArray[4][0])} mode="hitlist" votes={voteArray[4][1].length} onHoverEvent={updateHoveredPlayer} onClickEvent={castVote}/>
                 ) : null}
             </div>
         </div>
