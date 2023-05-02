9441d12ea0d47b48aaafd2b9c773ca75cbbf1194
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2023-05-02 21:33:41 +0200

    #closes 88, now at every point in the game the picture should reflect in what stage we currently are (day or night)

--- src/components/ui/game/Endscreen.js
+++ src/components/ui/game/Endscreen.js
@@ -62,7 +62,7 @@ const Endscreen = ({ endData, lobby, stage}) => {
         </div>
 
         <div className='endscreen-winner'>
-          <img src={`/static/media/${endData.winner}.png`} alt='Winning Team'></img>
+          <img src={`/static/media/${endData.winner}-${(stage === 'Day' ? "light" : "dark")}.png`} alt='Winning Team'></img>
         </div>
         <h5>Winners</h5>
         <div className='endscreen-winner-players'>
