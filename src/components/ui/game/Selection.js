33a64c1ad96dca4ebaffdeaaaf35d2684026bea3
djscherrer <davidjscherrer@outlook.com>
2023-05-04 10:08:49 +0200

    Changes to selection

--- src/components/ui/game/Selection.js
+++ src/components/ui/game/Selection.js
@@ -32,44 +32,40 @@ const Selection = ({currentPoll}) => {
   let content;
  
   switch (currentPoll.role) {
-
     case 'Werewolf':
       content = (
         <>
         <div className="game-player-selection-wrapper">
-          {currentPoll.participants.map(player => (
-            (!voteParticipantIds.includes(player.id) && player.alive) && (
-              <Profile user={new Player(player)} mode="selection-small" onClickEvent={currentPoll.participants.length > 0 ? castVote : ""} key={player.id} />
+          {currentPoll.pollOptions.map(option => (
+            (!voteParticipantIds.includes(option.player.id)) && (
+              <Profile user={new Player(option.player)} mode="selection-small" onClickEvent={castVote} key={option.player.id} />
             )
           ))}
         </div>
         <h2>Your fellow werewolves:</h2>
         <div className="game-player-selection-wrapper">
-          {currentPoll.participants.map(player => {
-            return (voteParticipantIds.includes(player.id) && player.alive) && (
-              <Profile user={new Player(player)} mode="selection-small" onClickEvent={currentPoll.participants.length > 0 ? castVote : ""} key={player.id} />
-            );
-          })}
+          {currentPoll.pollOptions.map(option => (
+            (voteParticipantIds.includes(option.player.id)) && (
+              <Profile user={new Player(option.player)} mode="selection-small" onClickEvent={castVote} key={option.player.id} />
+            )
+          ))}
         </div>
         </>
       );
       break;
-    case 'Villager':
+    default:
         content = (
           <>
             <div className="game-player-selection-wrapper">
               {currentPoll.pollOptions.map(option => {
                 return (
-                  <Profile user={new Player(option.player)} mode={selectionSize} onClickEvent={currentPoll.participants.length > 0 ? castVote : ""} key={option.player.id} />
+                  <Profile user={new Player(option.player)} mode={selectionSize} onClickEvent={castVote} key={option.player.id} />
                 );
               })}
             </div>
           </>
         );
       break;
-    default:
-      content = <Spinner />;
-    break;
   }
 
   return (
