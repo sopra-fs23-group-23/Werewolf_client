c1cb1329b7b72e1f12f8b2bdb7f1fff156d7f955
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2023-05-11 11:50:45 +0200

    The game log now gets refreshed if you join a new game and no longer shows the information of the last game

--- src/hooks/Game.hooks.js
+++ src/hooks/Game.hooks.js
@@ -12,7 +12,7 @@ const periodicFunctionCaller = () => {
   periodicFunctionToBeCalled();
 }
 
-const logger = new Log();
+let logger = new Log();
 
 let gameShouldBeFetchedAgain = false;
 
@@ -123,6 +123,7 @@ export const useGame = () => {
 
     useEffect(() => {
       setTimeout(async () => {
+        logger = new Log();
         await fetchGame();
         await fetchPoll();
         setStarted(true);
