9a5db012dd85faf293b0551d5e20570cc92db6b9
djscherrer <davidjscherrer@outlook.com>
2023-05-14 14:44:46 +0200

    Tried something with video and img

--- src/components/ui/Profile.js
+++ src/components/ui/Profile.js
@@ -21,21 +21,22 @@ const Profile = ({ user, mode, votes, onClickEvent, onHoverEvent }) => {
   // image.src = user.avatarUrl;
   // image.alt = 'avatar';
 
-  // function updateImageDisplay() {
-  //   var container = document.getElementById(`profile-video-${user.id}`);
-  //   if (!StorageManager.getIsVideoEnabled()) {
-  //     container.insertBefore(image, container.firstChild);
-  //   } else {
-  //     container.removeChild(image);
-  //   }
-  // }
-  // updateImageDisplay();
-  // setInterval(updateImageDisplay, 1000);
+  function updateImageDisplay() {
+    var video = document.getElementById(`profile-video-${user.id}`);
+    var image = document.getElementById(`profile-image-${user.id}`);
+    if (StorageManager.getIsVideoEnabled()) {
+      image.setAttribute('hidden', 'true');
+    } else {
+      video.setAttribute('hidden', 'true');
+    }
+  }
+  updateImageDisplay();
+  setInterval(updateImageDisplay, 1000);
 
   return (
     <div className={`profile profile-${mode}`} id={`profile-${mode}-${user.id}`} onClick={handleClick}>
-      <div className={`profile-${mode}-video`} id={`profile-video-${user.id}`} onMouseEnter={handleHover(user)} onMouseLeave={handleHover(null)}></div>
-      {/*<img className={`video profile-${mode}-video`} id={`profile-video-${user.id}`} onMouseEnter={handleHover(user)} onMouseLeave={handleHover(null)} src={user.avatarUrl} alt='avatar'/>*/}
+      <div className={`video profile-${mode}-video`} id={`profile-video-${user.id}`} onMouseEnter={handleHover(user)} onMouseLeave={handleHover(null)}></div>
+      <img className={`video profile-${mode}-image`} id={`profile-image-${user.id}`} onMouseEnter={handleHover(user)} onMouseLeave={handleHover(null)} src={user.avatarUrl} alt='avatar'/>
       <div className="profile-name">{user.name}</div>
       {votes && (
         <h2 className="profile-votes">
