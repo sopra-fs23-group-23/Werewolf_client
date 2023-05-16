295413a2804b105dafc12665940a12bdc2265916
Atomis14 <michelsabbatini@outlook.com>
2023-05-16 20:50:09 +0200

    #98 send duration update to server

--- src/components/ui/DurationSelection.js
+++ src/components/ui/DurationSelection.js
@@ -1,24 +1,68 @@
+import { useEffect, useState } from 'react';
+import { api } from 'helpers/api';
+import StorageManager from 'helpers/StorageManager';
 import 'styles/ui/DurationSelection.scss';
 
 const DurationSelection = () => {
+  const [singleVoteDuration, setSingleVoteDuration] = useState('');
+  const [partyVoteDuration, setPartyVoteDuration] = useState('');
+
+  const handleSingleVoteChange = async (duration) => {
+    try {
+      await api.put('/lobbies/' + StorageManager.getLobbyId() + '/settings', {
+        singleVoteDurationSeconds: duration,
+      });
+      setSingleVoteDuration(duration);
+    } catch(e) {
+      alert('Could not update vote duration.');
+      console.error(e);
+    }
+  }
+
+  const handlePartyVoteChange = async (duration) => {
+    try {
+      await api.put('/lobbies/' + StorageManager.getLobbyId() + '/settings', {
+        partyVoteDurationSeconds: duration,
+      });
+      setPartyVoteDuration(duration);
+    } catch(e) {
+      alert('Could not update vote duration.');
+      console.error(e);
+    }
+  }
+
+  // fetch initial values
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await api.get('/lobbies/' + StorageManager.getLobbyId() + '/settings');
+        setSingleVoteDuration(response.data.singleVoteDurationSeconds);
+        setPartyVoteDuration(response.data.partyVoteDurationSeconds);
+      } catch(e) {
+        alert('Could not fetch lobby durations.');
+        console.error(e);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="duration-selection">
       <div>
         <label>Single Vote Duration</label>
-        <select value={15}>
+        <select value={singleVoteDuration} onChange={e => handleSingleVoteChange(e.target.value)}>
           <option value="15">15 Seconds</option>
           <option value="30">30 Seconds</option>
-          <option value="90">90 Seconds</option>
-          <option value="90">120 Seconds</option>
+          <option value="60">60 Seconds</option>
         </select>
       </div>
       <div>
         <label>Party Vote Duration</label>
-        <select value={90}>
+        <select value={partyVoteDuration} onChange={e => handlePartyVoteChange(e.target.value)}>
           <option value="15">15 Seconds</option>
           <option value="30">30 Seconds</option>
           <option value="90">90 Seconds</option>
-          <option value="90">120 Seconds</option>
+          <option value="120">120 Seconds</option>
         </select>
       </div>
     </div>
