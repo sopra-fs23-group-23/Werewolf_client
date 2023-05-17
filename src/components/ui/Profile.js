49ba98c7341c8dd47ccd065c835d474bb34fc158
Miroshima <miro.vannini@protonmail.ch>
2023-05-17 21:47:30 +0200

    egg

--- src/components/ui/Profile.js
+++ src/components/ui/Profile.js
@@ -16,7 +16,7 @@ const Profile = ({ user, mode, votes, onClickEvent, onHoverEvent }) => {
 
   return (
     <div className={`profile profile-${mode}`} id={`profile-${mode}-${user.id}`} onClick={handleClick}>
-      <div className={`video profile-${mode}-video`} id={`profile-video-${user.id}`} onMouseEnter={handleHover(user)} onMouseLeave={handleHover(null)} hidden></div>
+      <div className={`video profile-${mode}-video`} id={`profile-video-${user.id}`} onMouseEnter={handleHover(user)} onMouseLeave={handleHover(null)} hidden/>
       <img className={`image profile-${mode}-image`} id={`profile-image-${user.id}`} onMouseEnter={handleHover(user)} onMouseLeave={handleHover(null)} src={user.avatarUrl} alt='avatar'/>
       <div className="profile-name">{user.name}</div>
       {votes && (
